ada1882ece937dd96329049c562fe12491c03ab2
홍지훈 <59546676+winz1108@users.noreply.github.com>
2026-03-01 17:33:28 +0100

    Updated main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import BinanceFuturesDashboard from './pages/BinanceFuturesDashboard';
 import FuturesDashboard from './pages/FuturesDashboard';
 
 function App() {
-  return <FuturesDashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/binance" replace />} />
+        <Route path="/binance" element={<BinanceFuturesDashboard />} />
+        <Route path="/futures" element={<FuturesDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
